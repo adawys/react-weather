8bb6df47989cfa0abda7e0204aaeb57db2e06416
Adam Wyszyński <adawys20@gmail.com>
2019-10-14 08:45:27 +0200

    Updates

---
+++ precache-manifest.b99357babcd6053e80b723a63cf58296.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "622925b2323e30b8fe9a6f3259fbe75a",
+    "url": "/react-weather/index.html"
+  },
+  {
+    "revision": "09a23280a837bd08a1e2",
+    "url": "/react-weather/static/css/main.9a581c19.chunk.css"
+  },
+  {
+    "revision": "179023842620d42d21bf",
+    "url": "/react-weather/static/js/2.8c3176c8.chunk.js"
+  },
+  {
+    "revision": "09a23280a837bd08a1e2",
+    "url": "/react-weather/static/js/main.0161e209.chunk.js"
+  },
+  {
+    "revision": "917bcefd0461ec358581",
+    "url": "/react-weather/static/js/runtime-main.940a6aa2.js"
+  }
+]);
